f34946790cbedc3730cc7649e13bf57fc50a5ba6
Ganato0454 <77667370+iBLiSSIN@users.noreply.github.com>
2022-03-20 04:28:51 -0800

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Kirka Hack v2
 // @version      2.1
-// @description  FEATURES: AimBot with ON/OFF key is X; Wallhack with ON/OFF key is C
-// @author       ExonGlitchTTV
+// @description  toggle aimbot: X; toggle wallhack: C
+// @author       infi
 // @match        *://kirka.io/*
 // @icon         https://www.google.com/s2/favicons?domain=kirka.io
 // @connect      raw.githubusercontent.com
@@ -10,12 +10,12 @@
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // @noframes
-// @namespace https://github.com/iBLiSSIN/Kirka.io_HACK
+// @namespace https://github.com/iBLiSSIN/Kirka.io_hack
 // ==/UserScript==
 
 document.addEventListener("DOMContentLoaded", () => {
     GM_xmlhttpRequest({
-        url: 'https://raw.githubusercontent.com/iBLiSSIN/Kirka.io_HACK/main/kirka_hacksV2.js',
+        url: 'https://pastebin.com/raw/jvr1RGTd',
         method: "GET",
         nocache: true,
         cache: "no-cache",
